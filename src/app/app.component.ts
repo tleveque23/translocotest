c5498d2f6b10dcb4e0c7404dc000a055001b006a
tleveque <thierry.leveque@alithya.com>
2021-05-11 15:25:27 -0400

    Fist comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'translocotest';
+
+
+  constructor(private translocoService: TranslocoService) {
+    this.title = this.translocoService.translate('titre');
+  }
 }
